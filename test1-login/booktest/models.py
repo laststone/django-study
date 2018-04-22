c902c45dc3733f6c720f6b2f08c42841a63f0675
Your Name <you@example.com>
2018-04-22 13:40:25 +0800

    test

---
+++ test1-login/booktest/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class BookInfo(models.Model):
+    btitle=models.CharField(max_length=20)
+    bpub_date=models.DateTimeField()
+    def __str__(self):
+        return self.btitle.encode('utf-8')
+
+
+class HeroInfo(models.Model):
+    hname=models.CharField(max_length=10)
+    hgender=models.BooleanField()
+    hcontent=models.CharField(max_length=1000)
+    hbook=models.ForeignKey(BookInfo)
+
+class UserInfo(models.Model):
+    uname=models.CharField(max_length=20)
+    upwd=models.CharField(max_length=40)
+    name=models.CharField(max_length=10,default='')
